abf5210f97125f5007f8311cd8571907aafae4ec
deepuguptacp <199deepu@gmail.com>
2024-04-10 21:48:38 +0530

    Changes

---
+++ app/components/navbar/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      NavBar
+    </div>
+  );
+};
+
+export default Sidebar;
