eb7c7a64580bc064a15a15d8d546f6f8d409157e
deepuguptacp <199deepu@gmail.com>
2024-04-11 09:09:44 +0530

    NavBar added

--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -1,12 +1,67 @@
-import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { NavItems } from "@/app/constants";
 import "./index.css";
 
-const Sidebar = () => {
+const NavBar = () => {
+  const [nav, setNav] = useState<boolean>(false);
+
   return (
-    <div className="sidebar">
-      NavBar
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black nav border-b-2">
+      <div className="border-bottom">
+        <h1 className="text-3xl font-signature ml-2">
+          <a
+            className="link-underline link-underline-black"
+            href=""
+            rel="noreferrer"
+          >
+            <Image
+              src="/svgIcons/home.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={24}
+              height={24}
+              priority
+            />
+          </a>
+        </h1>
+      </div>
+
+      <ul className="hidden md:flex">
+        {NavItems.map(({ id, link }) => (
+          <li
+            key={id}
+            className="nav-links px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 hover:text-white duration-200 link-underline"
+          >
+            <Link href={link}>{link}</Link>
+          </li>
+        ))}
+      </ul>
+
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+          {NavItems.map(({ id, link }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl"
+            >
+              <Link onClick={() => setNav(!nav)} href={link}>
+                {link}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
 
-export default Sidebar;
+export default NavBar;
---
+++ app/constants.tsx
@@ -0,0 +1,22 @@
+export const NavItems = [
+  {
+    id: 1,
+    link: "home",
+  },
+  {
+    id: 2,
+    link: "about",
+  },
+  {
+    id: 3,
+    link: "portfolio",
+  },
+  {
+    id: 4,
+    link: "experience",
+  },
+  {
+    id: 5,
+    link: "contact",
+  },
+];
