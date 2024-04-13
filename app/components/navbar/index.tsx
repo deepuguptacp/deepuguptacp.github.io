556a3879cb7cc79e8850a94650c6052700919cdf
deepuguptacp <199deepu@gmail.com>
2024-04-13 21:14:03 +0530

    App router added

--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 import { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { NavItems } from "@/app/constants";
-import "./index.css";
 
 const NavBar = () => {
   const [nav, setNav] = useState<boolean>(false);
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
 "use client";
+import Image from "next/image";
 import { NavBar } from "./components";
 
 export default function Home() {
   return (
     <main className="flex flex-col justify-between">
       <NavBar />
-      <div>
-        Deepu Gupta
+      <div className="flex items-center min-h-lvh">
+        <div className="">
+          <Image
+            src="/images/profileImage2.jpeg"
+            alt="Vercel Logo"
+            className="dark m-10 border-b-2 rounded-full"
+            width={300}
+            height={350}
+            priority
+          />
+        </div>
+        <div>Hello</div>
       </div>
     </main>
   );
